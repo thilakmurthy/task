356a353b429fbe685674158ecc361e4af3195817
thilakmurthy <71584170+thilakmurthy@users.noreply.github.com>
2020-09-20 13:14:19 +0530

    Add files via upload

---
+++ ipl dbms/rank_u.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<style >
+	table{
+		border: 10px solid white;
+		color:black;
+	}
+	tr{
+		border: 10px solid white;
+		background-color:#310F19;
+	}
+	th{
+		border: 10px solid white;
+		color: white;
+	}
+</style>
+<head>
+	<title>RANKS</title>
+</head>
+<body style="background-color:">
+	<button style="background-color:#33FF76 "><a href="user1st.html" style="color:">Back</a></button>
+
+	 <div style="margin-top:10px; style : center" ><table width="100%"><tr><th><p style="align-content: center;"><h1 style="color:">TEAM RANKING</h1></p></th></tr><tr><th>
+	<table align="center">
+		<tr>
+			<th>Rank</th>
+			<th>Name</th>
+			<th>Rating</th>
+		</tr>
+		<?php
+		$con=mysqli_connect("localhost","root","","cricket");
+		
+
+
+
+		$query="select * from team order by rating desc";
+		$result=mysqli_query($con,$query);
+		[$i]=floor(1);
+		if(mysqli_num_rows($result)>0)
+		{
+			while ($row=mysqli_fetch_assoc($result)) { $i=$i+1;
+			
+			//if($rate!=floor($i))
+			//	{$q="update team set rank='floor($i)' where rating='$rating'";
+    		//	mysqli_query($con,$q);
+			
+
+			echo "<tr><th>"
+			.floor($i)."</th><th>".
+			$row["name"]."</th><th>".
+			$row["rating"]."</th></tr>";
+			}}
+		?>
+</table></th></tr></table></div>
+
+
+
+
+
+
+<table width="100%"><tr><th>
+ <div style="margin-top:10px; style : center" >
+ 	<table width="100%"><tr><th><p style="align-content: center;"><h1 style="color: #33EBFF">BATSMAN RANKING</h1></p></th></tr>
+	<table align="center">
+		<tr>
+			<th>Name</th>
+			<th>Rank</th>
+			<th>Teamname</th>
+			<th>Runs</th>
+		</tr>
+		<?php
+		$con=mysqli_connect("localhost","root","","cricket");
+		$query="select * from player  where type='batsman' order by runs desc";
+		$result=mysqli_query($con,$query);[$i]=floor(1);
+		if(mysqli_num_rows($result)>0)
+		{
+			while ($row=mysqli_fetch_assoc($result)) { $i=$i+1;
+			echo "<tr><th>".$row["playername"]."</th><th>".
+			floor($i)."</th><th>".
+			$row["name"]."</th><th>".
+			$row["runs"]."</th></tr>";
+			}
+		}?></table></th><th>
+
+
+
+
+<table width="100%"><tr style="width: 100%"><th>
+<p align="center">
+	<h1> BOWLER RANKING
+</h1>
+</p></th></tr>
+		<table align="center">
+		<tr>
+			<th>Name</th>
+			<th>Rank</th>
+			<th>Teamname</th>
+			<th>wickets</th>
+			
+		</tr>
+		<?php
+		$con=mysqli_connect("localhost","root","","cricket");
+		$query="select * from player  where type='bowler' order by wickets desc";
+		$result=mysqli_query($con,$query);[$i]=floor(1);
+		if(mysqli_num_rows($result)>0)
+		{
+			while ($row=mysqli_fetch_assoc($result)) { $i=$i+1;
+			echo "<tr><th>".$row["playername"]."</th><th>".
+			floor($i)."</th><th>".
+			$row["name"]."</th><th>".
+			$row["wickets"]."</th></tr>";
+			}
+		}?></table></th><th>
+		
+
+
+<table width="100%"><tr style="width: 100%"><th>
+<p align="center">
+	<h1>ALL-ROUNDER RANKING
+</h1>
+</p></th></tr>
+
+		<table align="center">
+		<tr>
+			<th>Name</th>
+			<th>Rank</th>
+			<th>Teamname</th>
+			<th>runs</th>
+			<th>wickets</th>
+		</tr>
+		<?php
+		$con=mysqli_connect("localhost","root","","cricket");
+
+		$query="select * from player  where type='allrounder' order by (runs+wickets*2) desc";
+		$result=mysqli_query($con,$query);[$i]=floor(1);
+		if(mysqli_num_rows($result)>0)
+		{
+			while ($row=mysqli_fetch_assoc($result)) { $i=$i+1;
+			echo "<tr><th>".$row["playername"]."</th><th>".	
+			floor($i)."</th><th>".
+			$row["name"]."</th><th>".
+			$row["runs"]."</th><th>".
+			$row["wickets"]."</th></tr>";
+			}
+		}
+	
+		mysqli_close($con);
+		?>
+	</table></th></tr></table></div></th></tr></table>
+	
+</th></tr></table>
+	
+</body>
+</html>
